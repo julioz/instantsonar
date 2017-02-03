126bf6d973b346e91ad1b13c3b93bc9fd97f8897
Julio Zynger <julio.zynger@soundcloud.com>
2017-02-03 18:23:26 +0100

    Initial structure for instant app

---
+++ sonarartists/src/main/java/com/sample/instantsonar/artists/ArtistsActivity.java
@@ -0,0 +1,13 @@
+package com.sample.instantsonar.artists;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArtistsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Hello World: Instant Apps");
+    }
+}
