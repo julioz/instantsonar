5bb60a1d61e62a19aa9e94755edbf082e3a0f262
Julio Zynger <julio.zynger@soundcloud.com>
2017-02-17 11:26:33 +0100

    Expose OkHttpClient dependency to be injected

--- sonarbase/src/main/java/com/sample/instantsonar/ApplicationComponent.java
+++ sonarbase/src/main/java/com/sample/instantsonar/ApplicationComponent.java
@@ -1,14 +1,12 @@
 package com.sample.instantsonar;
 
 import dagger.Component;
-
-import android.support.v4.app.Fragment;
+import okhttp3.OkHttpClient;
 
 import javax.inject.Singleton;
 
 @Singleton
 @Component(modules = {AppModule.class, NetworkModule.class})
 public interface ApplicationComponent {
-    void inject(SampleApplication app);
-    void inject(Fragment fragment);
+    OkHttpClient okHttpClient();
 }
