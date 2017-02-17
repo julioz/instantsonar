785baafd626e81f112321462f521af1a49d67548
Julio Zynger <julio.zynger@soundcloud.com>
2017-02-18 00:46:40 +0100

    Basic hello world label setting

--- sonarartists/src/main/java/com/sample/instantsonar/artists/ArtistsFragment.java
+++ sonarartists/src/main/java/com/sample/instantsonar/artists/ArtistsFragment.java
@@ -3,21 +3,18 @@ package com.sample.instantsonar.artists;
 import com.sample.instantsonar.SampleApplication;
 import com.soundcloud.lightcycle.LightCycle;
 import com.soundcloud.lightcycle.LightCycleSupportFragment;
-import okhttp3.OkHttpClient;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
 public class ArtistsFragment extends LightCycleSupportFragment<ArtistsFragment> {
 
     @Inject @LightCycle ArtistsPresenter presenter;
-
-    @Inject OkHttpClient client;
 
     public static ArtistsFragment newInstance() {
         return new ArtistsFragment();
@@ -31,12 +28,12 @@ public class ArtistsFragment extends LightCycleSupportFragment<ArtistsFragment> 
                 .inject(this);
     }
 
-    public static final String TAG = "ArtistsFragment";
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_artists, null);
-        Log.e(TAG, "onCreateView: " + client);
-        return view;
+        return inflater.inflate(R.layout.fragment_artists, null);
+    }
+
+    public void setContent(String content) {
+        ((TextView) getView().findViewById(R.id.fragment_artists_content)).setText(content);
     }
 }
--- sonarartists/src/main/java/com/sample/instantsonar/artists/ArtistsPresenter.java
+++ sonarartists/src/main/java/com/sample/instantsonar/artists/ArtistsPresenter.java
@@ -3,44 +3,34 @@ package com.sample.instantsonar.artists;
 import com.soundcloud.lightcycle.DefaultSupportFragmentLightCycle;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
 import javax.inject.Inject;
 
 class ArtistsPresenter extends DefaultSupportFragmentLightCycle<ArtistsFragment> {
 
-    private static final String TAG = ArtistsFragment.TAG;
+    private static final String TAG = "ArtistsPresenter";
+
+    private ArtistsFragment fragment;
 
     @Inject
-    public ArtistsPresenter() {}
+    public ArtistsPresenter() {
+    }
 
     @Override
     public void onCreate(ArtistsFragment fragment, Bundle bundle) {
-        Log.i(TAG, "Creating fragment:" + fragment);
+        this.fragment = fragment;
     }
 
     @Override
-    public void onStart(ArtistsFragment fragment) {
-        Log.i(TAG, "Starting fragment:" + fragment);
-    }
-
-    @Override
-    public void onResume(ArtistsFragment fragment) {
-        Log.i(TAG, "Resuming fragment:" + fragment);
-    }
-
-    @Override
-    public void onPause(ArtistsFragment fragment) {
-        Log.i(TAG, "Pausing fragment:" + fragment);
-    }
-
-    @Override
-    public void onStop(ArtistsFragment fragment) {
-        Log.i(TAG, "Stopping fragment:" + fragment);
+    public void onViewCreated(ArtistsFragment host, View view, Bundle savedInstanceState) {
+        super.onViewCreated(host, view, savedInstanceState);
+        fragment.setContent("Hello World");
     }
 
     @Override
     public void onDestroy(ArtistsFragment fragment) {
-        Log.i(TAG, "Destroying fragment:" + fragment);
+        this.fragment = null;
+        super.onDestroy(fragment);
     }
 }
