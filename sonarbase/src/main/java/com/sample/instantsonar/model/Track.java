a710ac2f6367df1b8171dbd806f95a852e2d8be8
Julio Zynger <julio.zynger@soundcloud.com>
2017-03-17 15:22:04 +0100

    Add fields to Track model

--- sonarbase/src/main/java/com/sample/instantsonar/model/Track.java
+++ sonarbase/src/main/java/com/sample/instantsonar/model/Track.java
@@ -6,12 +6,14 @@ public class Track {
     private long id;
     private long duration;
     private String title;
-    @SerializedName("artwork_url")
-    private String artworkUrl;
-    @SerializedName("playback_count")
-    private long playbackCount;
-    @SerializedName("favoritings_count")
-    private long favoritingsCount;
+    @SerializedName("artwork_url") private String artworkUrl;
+    private String description;
+    private String genre;
+    @SerializedName("playback_count") private long playbackCount;
+    @SerializedName("favoritings_count") private long favoritingsCount;
+    @SerializedName("waveform_url") private String waveformUrl;
+    @SerializedName("stream_url") private String streamUrl;
+    private User user;
 
     public long getId() {
         return id;
@@ -29,12 +31,48 @@ public class Track {
         return artworkUrl;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
     public long getPlaybackCount() {
         return playbackCount;
     }
 
     public long getFavoritingsCount() {
         return favoritingsCount;
+    }
+
+    public String getWaveformUrl() {
+        return waveformUrl;
+    }
+
+    public String getStreamUrl() {
+        return streamUrl;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Track track = (Track) o;
+
+        return id == track.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
     }
 
     @Override
@@ -44,8 +82,13 @@ public class Track {
                 ", duration=" + duration +
                 ", title='" + title + '\'' +
                 ", artworkUrl='" + artworkUrl + '\'' +
+                ", description='" + description + '\'' +
+                ", genre='" + genre + '\'' +
                 ", playbackCount=" + playbackCount +
                 ", favoritingsCount=" + favoritingsCount +
+                ", waveformUrl=" + waveformUrl +
+                ", streamUrl='" + streamUrl + '\'' +
+                ", user=" + user +
                 '}';
     }
 }
