e9fbc32d9c9bad77cc130a736fab91aac75767bc
Julio Zynger <julio.zynger@soundcloud.com>
2017-03-24 16:10:03 +0100

    Basic setup for unit tests

---
+++ sonarartists/src/test/java/com/sample/instantsonar/artists/ArtistsOperationsTest.java
@@ -0,0 +1,14 @@
+package com.sample.instantsonar.artists;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ArtistsOperationsTest {
+
+    @Test
+    public void abc() throws Exception {
+        fail();
+    }
+
+}
